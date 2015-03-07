fba450d8605a5c09006f0d38e331a95c513d5f4e
Tim Giblin <timmyg13@gmail.com>
2015-03-07 14:56:10 -0500

    update readme

--- examples/example.js
+++ examples/example.js
@@ -5,6 +5,6 @@ emailClient.setDefaultFrom("from@email.com");
 emailClient.configureSendgrid("venn-email", "Password123", 2);
 emailClient.configureMandrill("aNdLxLa4xFG4JR-wpeMklw", 1);
 
-emailClient.send("timmyg13@gmail.com", "subj", "msg", function(err, result){
+emailClient.send("test@email.com", "subj", "msg", function(err, result){
 	debug("done");
 });
