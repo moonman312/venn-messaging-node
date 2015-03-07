5e7488efec1d6d569a23394fadf6f8c170839319
Tim Giblin <timmyg13@gmail.com>
2015-03-07 13:43:35 -0500

    make api easier

--- examples/example.js
+++ examples/example.js
@@ -1,8 +1,7 @@
-EmailClient = require("../lib/index");
+emailClient = require("../lib/index");
 
-emailClient = new EmailClient();
 emailClient.setFrom("from@email.com");
-emailClient.configureSendgrid("venn-email", "Password123", 1);
+emailClient.configureSendgrid("venn-email", "Password123", 3);
 emailClient.configureMandrill("aNdLxLa4xFG4JR-wpeMklw", 2);
 
 emailClient.send("timmyg13@gmail.com", "subj", "msg", function(err, result){
