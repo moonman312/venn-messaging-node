7c35db55670ebb97dfe428dcb1e7e7cf47dd858e
Tim Giblin <timmyg13@gmail.com>
2015-04-27 17:03:03 -0400

    remove email address, bump to version 0.4

--- examples/example.js
+++ examples/example.js
@@ -2,6 +2,6 @@ var debug = require('debug')('email');
 var email = require("../lib/index");
 
 email.initialize()
-email.send("from@email.com", "timmyg13@gmail.com", "subject-1", "message-1", function(err, result){
+email.send("from@email.com", "testy@email.com", "subject-1", "message-1", function(err, result){
 	console.info("send with:", result);
 })
