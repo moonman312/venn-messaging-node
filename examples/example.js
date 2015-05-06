06b862d43d65586ace037d1e6c87b05d8f768737
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-05-06 17:21:54 -0400

    Reset example.

--- examples/example.js
+++ examples/example.js
@@ -8,34 +8,6 @@ var data = {
 	subject: "subject-1",
 	message: "message-1"
 }
-/*Email.send(data, function(err, result){
+Email.send(data, function(err, result){
 	console.info("sent with:", result);
-})*/
-
-// Testing
-exceedLimit = function (count) {
-
-	var mail = {
-		from: 'from@venn.com',
-		to: 'nathan@getvenn.io',
-		subject: 'subject-' + count.toString(),
-		message: 'message-' + count.toString()
-	}
-
-	console.log(count.toString());
-
-	Email.send(mail, function (err, result) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.info('sent with:', result);
-		}
-	})
-
-	if (count < 300) {
-		setTimeout(function() {
-			exceedLimit(count + 1)
-		}, 1000)
-	}
-}
-exceedLimit(300);
+})
