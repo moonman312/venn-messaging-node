6fd180583019750d2920afdc450dc1befa595d2d
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-05-07 17:32:21 -0400

    Testing LIMIT_EXCEEDED error for Twilio API. fixes #21

---
+++ test/sms/limit_exceeded.js
@@ -0,0 +1,31 @@
+var assert = require("assert")
+var nock = require("nock")
+var client = require("../../lib/index").SMS;
+var ErrorCode = require('../../lib/models/messaging_error_code');
+
+describe('sms services should provide feedback when user exceeds sending limit', function() {
+
+	it('should catch a limit exceeded error from Twilio', function (done) {
+
+		nock('https://api.getvenn.io/v1')
+			.get('/keys/sms')
+			.reply(200, {
+				"twilio": {
+					"account_sid": "sldkfjdslkjf",
+					"auth_token": "sldkfjdslkjf"
+				}
+			});
+		nock('https://api.twilio.com:443')
+			.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
+			.reply(401, {'status': 401, 'message': 'Authenticate', 'code': 20005, 'moreInfo': 'https://www.twilio.com/docs/errors/20005'});
+		nock('https://api.getvenn.io/v1')
+			.get('/priority/sms')
+			.reply(200, ["twilio"]);
+
+		client.initialize()
+		client.send({from:"+15138853322", to:"+15138853322", message:"message-13579"}, function(err, result){
+			assert.equal(this.sendLog[0].code, ErrorCode.LIMIT_EXCEEDED);
+			done()
+		})
+	})
+})
