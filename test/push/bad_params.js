a7d1ff507dfa169e6e7cc082eb9cafeb77a9c615
Tim Giblin <timmyg13@gmail.com>
2015-05-11 20:56:16 -0400

    fix broken tests

--- test/push/bad_params.js
+++ test/push/bad_params.js
@@ -6,7 +6,7 @@ describe('when push services up', function(){
 
 	it('should show error if incorrect params', function(done){
 		client.initialize()
-		client.send({deviceType:"ios", deviceToken: "sdfsfd", message:"message-13579"}, function(err, result){
+		client.send({deviceType:"apple", deviceToken: "sdfsfd", message:"message-13579"}, function(err, result){
 			should.exist(err);
 			should.not.exist(result);
 			err.message.should.equal("Invalid parameters!")
--- test/sms/fallback.js
+++ test/sms/fallback.js
@@ -27,11 +27,11 @@ describe('sending an sms should fallback', function(){
 				.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
 				.reply(500, [{"status": "sent"}] );
 			nock('https://rest.nexmo.com')
-				.post('/sms/json?from=15139453300&to=testy@email.com&text=message-1&api_key=nsldkfjdslkjf&api_secret=nsldkfjdslkjf')
+				.post('/sms/json?from=15139453300&to=15129435544&text=message-1&api_key=nsldkfjdslkjf&api_secret=nsldkfjdslkjf')
 				.reply(200, {"message": "success"});
 
 			emailClient.initialize(process.env.VENN_API_KEY)
-			emailClient.send({from:"from@email.com", to:"testy@email.com", message:"message-1"}, function(err, result){
+			emailClient.send({to:"15129435544", message:"message-1"}, function(err, result){
 				assert.equal(result.service, "nexmo");
 				done()
 			})
@@ -60,7 +60,7 @@ describe('sending an sms should fallback', function(){
 				.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
 				.reply(200, [{"status": "sent"}] );
 			nock('https://rest.nexmo.com')
-				.post('/sms/json?from=15139453300&to=testy@email.com&text=message-1&api_key=nsldkfjdslkjf&api_secret=nsldkfjdslkjf')
+				.post('/sms/json?from=15139453300&to=15129435544&text=message-1&api_key=nsldkfjdslkjf&api_secret=nsldkfjdslkjf')
 				.reply(400, {
 				  "message-count":"1",
 				  "status":"2",
@@ -73,7 +73,7 @@ describe('sending an sms should fallback', function(){
 				});
 
 			emailClient.initialize(process.env.VENN_API_KEY)
-			emailClient.send({from:"from@email.com", to:"testy@email.com", message:"message-1"}, function(err, result){
+			emailClient.send({to:"15129435544", message:"message-1"}, function(err, result){
 				assert.equal(result.service, "twilio");
 				done()
 			})
