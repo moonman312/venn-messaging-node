03dc96174e27a63c3b83192d38fe018b2b36749d
Tim Giblin <timmyg13@gmail.com>
2015-05-13 00:02:50 -0400

    add tests for twilio erros

--- test/sms/twilio.js
+++ test/sms/twilio.js
@@ -110,4 +110,72 @@ describe('twilio sms', function(){
 		})
 	})
 
+	describe('when bad to number', function(done){
+
+		it('should return an error', function(done){
+			nock.cleanAll()
+			nock('https://api.getvenn.io/v1')
+				.get('/keys/sms')
+				.reply(200, {
+					"twilio": {
+						"account_sid": "sldkfjdslkjf",
+						"auth_token": "sldkfjdslkjf"
+					},
+					"nexmo": {
+						"api_key": "sldkfjdslkjf",
+						"api_secret": "sldkfjdslkjf"
+					}
+				});
+			nock('https://api.twilio.com:443')
+				.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
+				.reply(201, [{"status": "sent"}] );
+			nock('https://rest.nexmo.com/sms/json')
+				.post('')
+				.reply(200, {"message": "success"});
+			nock('https://api.getvenn.io/v1')
+				.get('/priority/sms')
+				.reply(200, [ "twilio"]);
+
+			client.initialize()
+			client.send({to:"15138853322923042903432", message:"message-13579"}, function(err, result){
+				assert.equal(result, undefined);
+				done()
+			})
+		})
+	})
+
+	describe('when bad from number', function(done){
+
+		it('should return an error', function(done){
+			nock.cleanAll()
+			nock('https://api.getvenn.io/v1')
+				.get('/keys/sms')
+				.reply(200, {
+					"twilio": {
+						"account_sid": "sldkfjdslkjf",
+						"auth_token": "sldkfjdslkjf"
+					},
+					"nexmo": {
+						"api_key": "sldkfjdslkjf",
+						"api_secret": "sldkfjdslkjf"
+					}
+				});
+			nock('https://api.twilio.com:443')
+				.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
+				.reply(201, [{"status": "sent"}] );
+			nock('https://rest.nexmo.com/sms/json')
+				.post('')
+				.reply(200, {"message": "success"});
+			nock('https://api.getvenn.io/v1')
+				.get('/priority/sms')
+				.reply(200, [ "twilio"]);
+
+			client.initialize()
+			client.send({to:"15135549122", from: "2309423098493840923", message:"message-13579"}, function(err, result){
+				assert.equal(result.service, undefined);
+				done()
+			})
+		})
+	})
+
 })
