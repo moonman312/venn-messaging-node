aa1fd90e36d4739817b59ce8eacc57b7ea8f2fa4
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-05-13 09:57:03 -0400

    Refactor Twilio tests to organize into normal and error categories. #27

---
+++ test/sms/twilio/error.js
@@ -0,0 +1,99 @@
+var assert = require("assert")
+var nock = require("nock")
+var client = require("../../../lib/index").SMS;
+
+describe('receive error from twilio', function () {
+
+	it('when no from number in code nor database', function (done) {
+		nock.cleanAll()
+		nock('https://api.getvenn.io/v1')
+			.get('/keys/sms')
+			.reply(200, {
+				"twilio": {
+					"account_sid": "sldkfjdslkjf",
+					"auth_token": "sldkfjdslkjf"
+				},
+				"nexmo": {
+					"api_key": "sldkfjdslkjf",
+					"api_secret": "sldkfjdslkjf"
+				}
+			});
+		nock('https://api.twilio.com:443')
+			.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
+			.reply(201, [{"status": "sent"}] );
+		nock('https://rest.nexmo.com/sms/json')
+			.post('')
+			.reply(200, {"message": "success"});
+		nock('https://api.getvenn.io/v1')
+			.get('/priority/sms')
+			.reply(200, [ "twilio"]);
+
+		client.initialize()
+		client.send({to:"15138853322", message:"message-13579"}, function(err, result){
+			assert.equal(result.service, undefined);
+			done()
+		})
+	})
+
+	it('when bad to number', function (done) {
+		nock.cleanAll()
+		nock('https://api.getvenn.io/v1')
+			.get('/keys/sms')
+			.reply(200, {
+				"twilio": {
+					"account_sid": "sldkfjdslkjf",
+					"auth_token": "sldkfjdslkjf"
+				},
+				"nexmo": {
+					"api_key": "sldkfjdslkjf",
+					"api_secret": "sldkfjdslkjf"
+				}
+			});
+		nock('https://api.twilio.com:443')
+			.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
+			.reply(201, [{"status": "sent"}] );
+		nock('https://rest.nexmo.com/sms/json')
+			.post('')
+			.reply(200, {"message": "success"});
+		nock('https://api.getvenn.io/v1')
+			.get('/priority/sms')
+			.reply(200, [ "twilio"]);
+
+		client.initialize()
+		client.send({to:"15138853322923042903432", message:"message-13579"}, function(err, result){
+			assert.equal(result, undefined);
+			done()
+		})
+	})
+
+	it('when bad from number', function (done) {
+		nock.cleanAll()
+		nock('https://api.getvenn.io/v1')
+			.get('/keys/sms')
+			.reply(200, {
+				"twilio": {
+					"account_sid": "sldkfjdslkjf",
+					"auth_token": "sldkfjdslkjf"
+				},
+				"nexmo": {
+					"api_key": "sldkfjdslkjf",
+					"api_secret": "sldkfjdslkjf"
+				}
+			});
+		nock('https://api.twilio.com:443')
+			.post('/2010-04-01/Accounts/sldkfjdslkjf/Messages.json')
+			.reply(201, [{"status": "sent"}] );
+		nock('https://rest.nexmo.com/sms/json')
+			.post('')
+			.reply(200, {"message": "success"});
+		nock('https://api.getvenn.io/v1')
+			.get('/priority/sms')
+			.reply(200, [ "twilio"]);
+
+		client.initialize()
+		client.send({to:"15135549122", from: "2309423098493840923", message:"message-13579"}, function(err, result){
+			assert.equal(result.service, undefined);
+			done()
+		})
+	})
+})
